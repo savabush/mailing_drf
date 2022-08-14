7468f9815602ec414b4a8bf3083e8ed7140dcfdf
savabush <vatka1337@gmail.com>
2022-08-14 22:17:09 +0700

    MOD | Updated celery settings + redis

--- mailing/celery.py
+++ mailing/celery.py
@@ -1,7 +1,11 @@
 import os
+import dotenv
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notification_service.settings')
+env_file = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '.env')
+dotenv.load_dotenv(env_file)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mailing.settings')
 
 app = Celery('notification_service')
 
